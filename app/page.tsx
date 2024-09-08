dae3e23d130bfb221811914b15f460263cfecac2
AbhijitKhatua <a.k.piintu@gmail.com>
2024-09-08 19:50:41 +0530

    one page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <div className="flex justify-center items-center h-screen flex-col gap-4">
         <h1 className="text-4xl font-medium">Sprout 🌱</h1>
         <p>Know your garden grow your garden</p>
-        <Button>Explore</Button>
+        <Button asChild><Link href="/dashboard">Explore</Link></Button>
       </div>
     </main>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['upload.wikimedia.org', 'cdn.pixabay.com'],
+    },
+};
+
 
 export default nextConfig;
